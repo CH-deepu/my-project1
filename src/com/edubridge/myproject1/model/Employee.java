225be58a8135e8d18fac3eeb5fa824ba1ede3de0
deepu-2001 <chekkilladeepika2001@gmail.com>
2024-06-06 10:46:44 +0530

    first commit

---
+++ src/com/edubridge/myproject1/model/Employee.java
@@ -0,0 +1,25 @@
+package com.edubridge.myproject1.model;
+
+public class Employee {
+private int employeeId;
+private String employee;
+private long employeeMobile;
+public int getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+}
+public String getEmployee() {
+	return employee;
+}
+public void setEmployee(String employee) {
+	this.employee = employee;
+}
+public long getEmployeeMobile() {
+	return employeeMobile;
+}
+public void setEmployeeMobile(long employeeMobile) {
+	this.employeeMobile = employeeMobile;
+}
+}
